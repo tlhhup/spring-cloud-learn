4cd11ae9c204ec5a26fb81665c6e4e1fc050c8f2
tlh <137273278@qq.com>
2018-10-04 21:07:57 +0800

    1.集成scca项目的提供的springcloud的后台管理

---
+++ tlh-scc-ui-server/src/main/java/org/tlh/springcloud/ConfigServerUiApplication.java
@@ -0,0 +1,18 @@
+package org.tlh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huping
+ * @desc
+ * @date 18/10/4
+ */
+@SpringBootApplication
+public class ConfigServerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerUiApplication.class,args);
+    }
+
+}
