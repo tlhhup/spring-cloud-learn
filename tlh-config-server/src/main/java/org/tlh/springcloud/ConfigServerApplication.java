e771785f8b53e0899cfd7b1f31b56f364c25d1d1
tlh <137273278@qq.com>
2018-10-05 01:21:49 +0800

    config admin说明

--- tlh-config-server/src/main/java/org/tlh/springcloud/ConfigServerApplication.java
+++ tlh-config-server/src/main/java/org/tlh/springcloud/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package org.tlh.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author huping
  * @desc
  * @date 18/10/4
  */
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
--- tlh-eureka-server-noauth/src/main/java/org/tlh/springcloud/EurekaServerNoAuthApplication.java
+++ tlh-eureka-server-noauth/src/main/java/org/tlh/springcloud/EurekaServerNoAuthApplication.java
@@ -2,19 +2,19 @@ package org.tlh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author huping
  * @desc
- * @date 18/10/4
+ * @date 18/10/5
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class ConfigServerAdminApplication {
+public class EurekaServerNoAuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerAdminApplication.class,args);
+        SpringApplication.run(EurekaServerNoAuthApplication.class,args);
     }
 
 }
